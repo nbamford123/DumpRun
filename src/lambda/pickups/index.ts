5b2f3e4861cf0925a07d6f1a2d01ab516fb13418
Nathan <nbamford@slacktronix.com>
2024-10-04 08:13:54 -0700

    chore: added new lambdas to index

--- src/lambda/pickups/index.ts
+++ src/lambda/pickups/index.ts
@@ -1,8 +1,10 @@
 export {
   acceptPickup,
   availablePickups,
+  cancelAcceptedPickup,
   createPickup,
   getPickup,
+  getPickups,
   updatePickup,
   deletePickup,
 } from './pickupHandlers.js';
